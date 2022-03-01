cc37e431a2eefc80a244807049ecdf2f34ebfb16
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-01 10:02:48 +0100

    fix: import classes for db context

--- Models/Data/MovieCharacterDbContext.cs
+++ Models/Data/MovieCharacterDbContext.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using MovieChatacterAPI.Models.Domain;
 
 
 namespace MovieChatacterAPI.Models
