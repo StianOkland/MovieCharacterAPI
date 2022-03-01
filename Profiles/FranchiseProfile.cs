222a78da9c442d3558c5055b6370050355961ca4
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-03-01 09:33:36 +0100

    feat: add automapper and profiles

---
+++ Profiles/FranchiseProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MovieChatacterAPI.Models;
+using MovieChatacterAPI.Models.Domain;
+
+namespace MovieChatacterAPI.Profiles
+{
+    public class FranchiseProfile : Profile
+    {
+        public FranchiseProfile()
+        {
+            CreateMap<Franchise, FranchiseDTO>();
+        }
+    }
+}
