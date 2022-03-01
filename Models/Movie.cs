be3aabd01f1b4856ec753b452ffdcde8aea6ecc9
Stian Økland <stian.okland@no.experis.com>
2022-03-01 08:24:52 +0100

    FEAT: Added classes and seeding for db

---
+++ Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieChatacterAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string MovieTitle { get; set; }
+
+        [MaxLength(50)]
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+
+        [MaxLength(50)]
+        public string Director { get; set; }
+
+        [MaxLength(100)]
+        public string Picture { get; set; }
+
+        [MaxLength(100)]
+        public string Trailer { get; set; }
+        public int FranchiseId { get; set; }
+        public Franchise Franchise { get; set; }
+        public ICollection<Character> Characters { get; set; }
+
+    }
+}
